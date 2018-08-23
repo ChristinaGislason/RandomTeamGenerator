5dd7069860b4c0a5797062036f1b1b55961b67ce
Christina Gislason <mkcgislason@gmail.com>
2018-08-23 02:02:28 -0700

    created constructor functions, linked css to html

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ console.log('js loaded');
 //input desired number of people in teams
 //generate team
 
-//stage 1: object literal listing students
+var totalStudents = [];
+var totalTeams = [];
+
+//object literal listing students
 var students = [
   'Aaron',
   'Alissa',
@@ -26,26 +29,28 @@ var students = [
   'Richard',
   'Suzanne',
   'Tre'
-]
+];
 
 function Students(name) {
-  this.name;
+  this.name = name;
+  totalStudents.push(this);
 }
 
 function Teams(numOfStudents) {
   this.numberOfStudents = numberOfStudents;
-  this.assignTeams = [];
+  totalTeams.push(this);
 }
 
-//assign all students to teams based on number of students desired per team
+//assign students into teams based on number of students desired per team
 function assignTeams() {
-  
+  var teams = totalStudents / totalTeams;
+  if (teams > 0) {
+    
+  }
 }
-
 
 //render table
 //Add table header row that says:
-//'[class name] teams for [assignment/lab name]' 
+//'[class name] teams for [assignment/lab name]'
 //table body will display teams and student names
 //table footer will display total number of teams and total number of students
-
