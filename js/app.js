d795e4312e594606cb1ded289781940a536ebcee
Christina Gislason <mkcgislason@gmail.com>
2018-08-23 01:47:21 -0700

    scaffolding done, added form to index.html, styled form

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+console.log('js loaded');
+
+//input individual names
+//input desired number of people in teams
+//generate team
+
+//stage 1: object literal listing students
+var students = [
+  'Aaron',
+  'Alissa',
+  'Andrew',
+  'Brian',
+  'Carlos',
+  'Christina',
+  'Danul',
+  'Jeff',
+  'Jeremy',
+  'Jimmy',
+  'Kendra',
+  'Kris',
+  'Neth',
+  'Nic',
+  'Rebecca',
+  'Richard',
+  'Suzanne',
+  'Tre'
+]
+
+function Students(name) {
+  this.name;
+}
+
+function Teams(numOfStudents) {
+  this.numberOfStudents = numberOfStudents;
+  this.assignTeams = [];
+}
+
+//assign all students to teams based on number of students desired per team
+function assignTeams() {
+  
+}
+
+
+//render table
+//Add table header row that says:
+//'[class name] teams for [assignment/lab name]' 
+//table body will display teams and student names
+//table footer will display total number of teams and total number of students
+
